4e82475ae51076545a8de367c3e59fe4781ce4d7
nibw <nur.ismam13@gmail.com>
2021-08-19 23:07:44 +0700

    body container

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,28 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Aplikasi Hello World"),),
-        body: Center(child: Text("Hello World")),
+        appBar: AppBar(
+          title: Text("Aplikasi Hello World"),
+        ),
+        body: Center(
+            child: Container(
+                color: Colors.lightGreen,
+                width: 100,
+                height: 50,
+                child: Text(
+                  "saya sedang melatih kemampuan flutter saya",
+                  // textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontStyle: FontStyle.italic,
+                      fontSize: 11),
+                ))),
       ),
     );
   }
