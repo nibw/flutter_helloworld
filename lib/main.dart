cb0660cab8fc480ea2976dfdf32fb86e12778759
nibw <nur.ismam13@gmail.com>
2021-08-22 13:37:16 +0700

    row and column widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MyApp());
+  runApp(MyApp());
 }
 
+//====================CHAPTER 1 s/d 3====================
+// ====================text Widgets====================
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text("Aplikasi Hello World"),
+//         ),
+//         body: Center(
+//             child: Container(
+//                 color: Colors.lightGreen,
+//                 width: 100,
+//                 height: 50,
+//                 child: Text(
+//                   "saya sedang melatih kemampuan flutter saya",
+//                   // textAlign: TextAlign.center,
+//                   style: TextStyle(
+//                       color: Colors.white,
+//                       fontStyle: FontStyle.italic,
+//                       fontSize: 11),
+//                 ))),
+//       ),
+//     );
+//   }
+// }
+
+// ====================CHAPTER 4====================
+// ====================Row dan Column Widget====================
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -12,21 +44,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Aplikasi Hello World"),
+          title: Text("latihan Row dan Column"),
         ),
-        body: Center(
-            child: Container(
-                color: Colors.lightGreen,
-                width: 100,
-                height: 50,
-                child: Text(
-                  "saya sedang melatih kemampuan flutter saya",
-                  // textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontStyle: FontStyle.italic,
-                      fontSize: 11),
-                ))),
+        // body : column() => untuk text ke bawah, body : row() => untuk text ke samping
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("Contoh Text 1"),
+            Text("Contoh Text 2"),
+            Text("Contoh Text 3"),
+            Row(
+              children: <Widget>[
+                Text("Contoh Text 4"),
+                Text("Contoh Text 5"),
+                Text("Contoh Text 6"),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
